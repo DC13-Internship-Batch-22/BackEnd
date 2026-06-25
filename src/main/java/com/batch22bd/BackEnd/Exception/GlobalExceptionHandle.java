30de0b2a0ea1c026d84da507024015f070bc7946
huuthang-0355 <vohuuthang3110@gmail.com>
2026-06-25 16:38:23 +0700

    fix exception

--- src/main/java/com/batch22bd/BackEnd/Exception/GlobalExceptionHandle.java
+++ src/main/java/com/batch22bd/BackEnd/Exception/GlobalExceptionHandle.java
@@ -133,11 +133,6 @@ public class GlobalExceptionHandle {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException exception) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of("message", exception.getMessage()));
-    }
+
 
 }
